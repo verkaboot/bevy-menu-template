f54a1b98b8b51076b19f6200d0ff6e9ef57425bf
Verkaboot <verkaboot@gmail.com>
2022-05-09 17:37:58 -0600

    Use iyes_loopless for state updates

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ mod menu;
 mod state;
 
 use bevy::prelude::*;
-use bevy_editor_pls::*;
+use iyes_loopless::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(EditorPlugin)
         .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
         .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
-        .add_state(state::Main::Menu)
-        .add_system_set(SystemSet::on_enter(state::Main::Menu).with_system(menu::setup))
+        .add_loopless_state(state::Main::Menu)
+        .add_enter_system(state::Main::Menu, menu::setup)
+        .add_system_set(
+            ConditionSet::new()
+                .run_in_state(state::Main::Menu)
+                .with_system(menu::update)
+                .into(),
+        )
+        .add_exit_system(state::Main::Menu, menu::cleanup)
         .run();
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,1 +1,76 @@
-pub fn setup() {}
+use bevy::prelude::*;
+use iyes_loopless::state::NextState;
+
+use crate::state;
+
+pub struct MenuData {
+    button_entity: Entity,
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    let button_entity = commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Play",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        })
+        .id();
+
+    commands.insert_resource(MenuData { button_entity });
+}
+
+pub fn update(
+    mut commands: Commands,
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                commands.insert_resource(NextState(state::Main::Game))
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+pub fn cleanup(mut commands: Commands, menu_data: Res<MenuData>) {
+    commands.entity(menu_data.button_entity).despawn_recursive();
+}
